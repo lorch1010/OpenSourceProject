a1ea6d44c37cbca93bd800cffe869c2562561f49
Jack Chen <zixuchen@pdx.edu>
2016-07-29 22:43:44 -0700

    update ScriptController.java && doing testing

--- bin/script.sc
+++ bin/script.sc
@@ -1,6 +1,2 @@
-set charName = Me
-set charImage = male.jpg
-
-cg #{charImage}
 msg #{charName} Standing in front of this high school in which I'll be spending the next three years feels weird.
 
--- src/basic/ScriptController.java
+++ src/basic/ScriptController.java
@@ -1,4 +1,5 @@
 package basic;
+import java.lang.StringBuilder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,6 +16,10 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class ScriptController {
+	// fields
+	private Map<String, String> vars;
+	private String message;
+	
 	public ScriptController(String scriptFile){
 		readScript(scriptFile);
 	}
@@ -27,7 +32,8 @@ public class ScriptController {
 			String line;
 			
 			while((line = bufferedReader.readLine()) != null){
-				System.out.println(line);
+				//System.out.println(line);
+				displayMessage(line);
 			}
 			reader.close();
 		}catch(IOException e){
@@ -35,7 +41,55 @@ public class ScriptController {
 		}
 	}
 	
-	public void parseScript(String statement){
-		// TO-DO
+	public void displayMessage(String msgToDisplay){
+		StringTokenizer st = new StringTokenizer(msgToDisplay);
+		while(st.hasMoreTokens()){
+			System.out.println(st.nextToken());
+		}
+	}
+	
+	public void invokeScript(String file){
+		String obj = null, instruct = null;
+		String data = null, ops = null;
+		String msgToDisplay;
+		StringTokenizer st = new StringTokenizer(file);
+		while(st.hasMoreTokens()){
+			instruct = st.nextToken();
+			obj = st.nextToken();
+			data = st.nextToken();
+			ops = st.nextToken();
+			
+			if("msg".equals(instruct)){
+				msgToDisplay = parseMsg(obj);
+				break;
+			}
+			
+		}
+	}
+	
+	public String parseMsg(String statement){
+		StringBuilder param = new StringBuilder();
+		StringBuilder msg = new StringBuilder();
+		boolean start = false;
+		
+		for(int i=0;i<statement.length();i++){
+			char temp = statement.charAt(i);
+			
+			if(start){
+				if(temp == '}'){
+					msg.append(vars.get("#{" + param + "}"));
+					param.delete(0, param.length());
+					start = false;
+				}else{
+					param.append(temp);
+				}
+			}else if(temp == '#' && i + 1 < statement.length() && statement.charAt(i + 1) == '{'){
+				start = true;
+				i++;
+			}else{
+				msg.append(temp);
+			}
+		}
+		return msg.toString();
 	}
 }
